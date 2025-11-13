eacc2813df95c9b59ee73135bcf9d5d06b81f554
Max Konovalov <MaxKOYT@yandex.ru>
2025-11-13 13:43:32 +0300

    -fix

--- database.py
+++ database.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SQLITE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    SQLITE_URL,
+    connect_args={"check_same_thread": False},
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
